0f162ac43de55bdb39b62db1355892d53679df41
tekka <tekka.gaia@gmail.com>
2018-03-11 11:04:55 +0900

    import

---
+++ native_app/ParallelHybridApp/AppServer.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using SuperWebSocket;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace ParallelHybridApp
+{
+
+    public partial class AppServer : Form
+    {
+        public List<String> log_ary = new List<string>();
+        public static AppServer frm;
+        public List<WebSocketSession> session_ary = new List<WebSocketSession>();
+
+        SuperWebSocket.WebSocketServer server;
+
+        public AppServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frm = this;
+
+            //コンフィグオブジェクト作成
+            var rootConfig = new SuperSocket.SocketBase.Config.RootConfig();
+            var serverConfig = new SuperSocket.SocketBase.Config.ServerConfig()
+            {
+                Port = 2013,
+                Ip = "127.0.0.1",
+                MaxConnectionNumber = 100,
+                Mode = SuperSocket.SocketBase.SocketMode.Tcp,
+                Name = "SuperWebSocket Sample Server",
+                MaxRequestLength = 1024 * 1024 * 10
+            };
+
+            //サーバーオブジェクト作成＆初期化
+            server = new SuperWebSocket.WebSocketServer();
+            server.Setup(rootConfig, serverConfig);
+
+            //イベントハンドラの設定
+            //接続
+            server.NewSessionConnected += HandleServerNewSessionConnected;
+            //メッセージ受信
+            server.NewMessageReceived += HandleServerNewMessageReceived;
+            //切断        
+            server.SessionClosed += HandleServerSessionClosed;
+
+            //サーバー起動
+            server.Start();
+
+        }
+            
+
+        //接続
+        static void HandleServerNewSessionConnected(SuperWebSocket.WebSocketSession session)
+        {
+            frm.Invoke((MethodInvoker)delegate () {
+
+                frm.session_ary.Add(session);
+                frm.add_log(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), "接続");
+
+            });
+            
+        }
+
+        //メッセージ受信
+        static void HandleServerNewMessageReceived(SuperWebSocket.WebSocketSession session,
+                                                    string e)
+        {
+            frm.Invoke((MethodInvoker)delegate ()
+            {
+                MessageData recv = JsonConvert.DeserializeObject<MessageData>(e);
+
+                switch (recv.command)
+                {
+                    case "add_message_to_app":
+
+                        frm.add_log(recv.time, "受信: " + recv.message);
+
+                        break;
+                }
+
+            });
+
+        }
+
+        //切断
+        static void HandleServerSessionClosed(SuperWebSocket.WebSocketSession session,
+                                                    SuperSocket.SocketBase.CloseReason e)
+        {
+            if(frm != null)
+            {
+                frm.Invoke((MethodInvoker)delegate () {
+                    frm.add_log(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), "切断");
+                });
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frm = null;
+
+            server.Stop();
+
+        }
+        
+        public void add_log(string time, String log)
+        {
+            log = "[" + time + "] " + log + "\r\n";
+            this.txtMessage.AppendText(log);
+        }
+
+        //メッセージ送信
+        private void send_message_to_sessions(string message)
+        {
+            foreach(var session in session_ary)
+            {
+                MessageData send = new MessageData();
+
+                send.command = "add_message_to_browser";
+                send.message = message;
+                send.time = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                string send_str = JsonConvert.SerializeObject(send);
+
+                session.Send(send_str);
+
+                add_log(send.time, "送信:" + message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            send_message_to_sessions(this.txtSendMessage.Text);
+        }
+    }
+}
